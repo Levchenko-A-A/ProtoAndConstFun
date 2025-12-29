bb47aaa06f206f8e60f834b85658063c0787ed32
Андрей Левченков <lev_a_a@mail.ru>
2025-12-29 07:04:30 +0200

    deleted excess code

--- src/ForceConstructor.js
+++
@@ -1,8 +0,0 @@
-export function ForceConstructor(param1, param2, param3) {
-  if (!new.target) {
-    return new ForceConstructor(param1, param2, param3);
-  }
-  this.param1 = param1;
-  this.param2 = param2;
-  this.param3 = param3;
-}
